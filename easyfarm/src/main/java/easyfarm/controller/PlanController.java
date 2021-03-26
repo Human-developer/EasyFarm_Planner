f0fb9c4906129d810b3c907fa4262cf5cdbba53b
Human-developer <liouvnet0705@naver.com>
2021-03-26 17:52:19 +0900

    [EasyFarm_Planner] 기능 : 없음 작성자 : 박금성 파일 : addSpend.html, getSchedule.html 내용 : 화면구성

--- easyfarm/src/main/java/easyfarm/controller/PlanController.java
+++ easyfarm/src/main/java/easyfarm/controller/PlanController.java
@@ -7,10 +7,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PlanController {
 
-	@GetMapping("/plan")
-	public String planMain(Model model) {
-		
-		
-		return "views/plan/planMain";
+	/* 월켈린더조회 */
+	@GetMapping("/plan/getSchedule")
+	public String getSchedule(Model model) {
+		return "views/plan/getSchedule";
+	}
+	
+	/* 계획등록 */
+	@GetMapping("/plan/addSpend")
+	public String addSpend() {
+		return "views/plan/addSpend";
 	}
 }
