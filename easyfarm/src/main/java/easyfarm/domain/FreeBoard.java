bec5980c5d1e466970449c9f2f8b3b8e7b0dfde7
한강희 <hanabcde2@gmail.com>
2021-04-15 09:56:37 +0900

    [EasyFarm_Planner] 기능 : 댓글기능 오류수정,비밀번호찾기 오류수정 작성자 : 한강희 파일 : member/board/getBoard 내용 : 댓글이 안써지는 오류수정, 비밀번호찾기 text오류 수정

--- easyfarm/src/main/java/easyfarm/domain/FreeBoard.java
+++ easyfarm/src/main/java/easyfarm/domain/FreeBoard.java
@@ -1,7 +1,16 @@
 package easyfarm.domain;
 
 
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Getter
+@Setter
 public class FreeBoard {
+	
 	private int commentsNum;		//댓글번호
 	private String commentsBoard;	//댓글달 게시판
 	private String memberId;		//댓글 작성자 아이디
@@ -15,116 +24,5 @@ public class FreeBoard {
 	private String boardGetNum; 	//게시판 조회수
 	private String memberName; 		//게시판 작성자 이름
 	
-	
-	
-	public String getMemberName() {
-		return memberName;
-	}
-	public void setMemberName(String memberName) {
-		this.memberName = memberName;
-	}
-	public int getCommentsNum() {
-		return commentsNum;
-	}
-	public void setCommentsNum(int commentsNum) {
-		this.commentsNum = commentsNum;
-	}
-	public String getCommentsBoard() {
-		return commentsBoard;
-	}
-	public void setCommentsBoard(String commentsBoard) {
-		this.commentsBoard = commentsBoard;
-	}
-	public String getMemberId() {
-		return memberId;
-	}
-	public void setMemberId(String memberId) {
-		this.memberId = memberId;
-	}
-	public String getCommentsContent() {
-		return commentsContent;
-	}
-	public void setCommentsContent(String commentsContent) {
-		this.commentsContent = commentsContent;
-	}
-	public String getCommentsDate() {
-		return commentsDate;
-	}
-	public void setCommentsDate(String commentsDate) {
-		this.commentsDate = commentsDate;
-	}
-	public int getBoardNum() {
-		return boardNum;
-	}
-	public void setBoardNum(int boardNum) {
-		this.boardNum = boardNum;
-	}
-	public String getBoardTitle() {
-		return boardTitle;
-	}
-	public void setBoardTitle(String boardTitle) {
-		this.boardTitle = boardTitle;
-	}
-	public String getBoardContent() {
-		return boardContent;
-	}
-	public void setBoardContent(String boardContent) {
-		this.boardContent = boardContent;
-	}
-	public String getBoardDate() {
-		return boardDate;
-	}
-	public void setBoardDate(String boardDate) {
-		this.boardDate = boardDate;
-	}
-	public String getBoardId() {
-		return boardId;
-	}
-	public void setBoardId(String boardId) {
-		this.boardId = boardId;
-	}
-	public String getBoardGetNum() {
-		return boardGetNum;
-	}
-	public void setBoardGetNum(String boardGetNum) {
-		this.boardGetNum = boardGetNum;
-	}
-	
-	
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("FreeBoard [commentsNum=");
-		builder.append(commentsNum);
-		builder.append(", commentsBoard=");
-		builder.append(commentsBoard);
-		builder.append(", memberId=");
-		builder.append(memberId);
-		builder.append(", commentsContent=");
-		builder.append(commentsContent);
-		builder.append(", commentsDate=");
-		builder.append(commentsDate);
-		builder.append(", boardNum=");
-		builder.append(boardNum);
-		builder.append(", boardTitle=");
-		builder.append(boardTitle);
-		builder.append(", boardContent=");
-		builder.append(boardContent);
-		builder.append(", boardDate=");
-		builder.append(boardDate);
-		builder.append(", boardId=");
-		builder.append(boardId);
-		builder.append(", boardGetNum=");
-		builder.append(boardGetNum);
-		builder.append("]");
-		return builder.toString();
-	}
-	
-	
-	
-	
-	
-	
-
 	
 }
